fe1fde67f13a63bbecf62cab92aaff06867c3241
Renat Skosarev <skosarrrev@gmail.com>
2023-02-19 01:18:33 +0500

    Add handlers for bottom buttons

--- app/src/main/java/dev/skosarev/itdiving/MainActivity.kt
+++ app/src/main/java/dev/skosarev/itdiving/MainActivity.kt
@@ -1,6 +1,10 @@
 package dev.skosarev.itdiving
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import kotlin.system.exitProcess
@@ -10,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     private var microphoneButton: MaterialButton? = null
     private var reactionsButton: MaterialButton? = null
     private var exitButton: MaterialButton? = null
+    private var messagesButton: MaterialButton? = null
+    private var usersButton: MaterialButton? = null
+    private var randomSortButton: MaterialButton? = null
+
+    private var cameraIconState: Boolean = true
+    private var microphoneIconState: Boolean = true
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +30,43 @@ class MainActivity : AppCompatActivity() {
         microphoneButton = findViewById(R.id.btn_microphone)
         reactionsButton = findViewById(R.id.btn_reactions)
         exitButton = findViewById(R.id.btn_exit)
+        messagesButton = findViewById(R.id.btn_messages)
+        usersButton = findViewById(R.id.btn_users)
+        randomSortButton = findViewById(R.id.btn_sort)
+
+
+        cameraButton?.setOnClickListener {
+            if (cameraIconState) {
+                cameraButton?.setIconResource(R.drawable.videocam_slash_alt_28)
+                cameraButton?.setIconTintResource(R.color.background)
+                cameraButton?.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+            } else {
+                cameraButton?.setIconResource(R.drawable.videocam_28)
+                cameraButton?.setIconTintResource(R.color.white)
+                cameraButton?.backgroundTintList = ColorStateList.valueOf(0x19191a)
+            }
+            cameraIconState = !cameraIconState
+        }
+
+        microphoneButton?.setOnClickListener {
+            if (microphoneIconState) {
+                microphoneButton?.setIconResource(R.drawable.microphone_slash_28)
+                microphoneButton?.setIconTintResource(R.color.background)
+                microphoneButton?.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+            } else {
+                microphoneButton?.setIconResource(R.drawable.microphone_alt_28)
+                microphoneButton?.setIconTintResource(R.color.white)
+                microphoneButton?.backgroundTintList = ColorStateList.valueOf(0x19191a)
+            }
+            microphoneIconState = !microphoneIconState
+        }
+
+        reactionsButton?.setOnClickListener {
+            AlertDialog.Builder(this).setTitle("Привет").setMessage("Прекрасного дня! ❤️").show()
+        }
 
         exitButton?.setOnClickListener {
+            Toast.makeText(applicationContext, "До свидания!", Toast.LENGTH_SHORT).show()
             finish()
             exitProcess(0)
         }
